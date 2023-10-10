21209348e7f337a960932deb87f8a150084d238a
joear <joear@Josephine>
2023-10-10 01:23:17 -0700

    Pushing Assignemnt1 and 2 to Centralized repository

---
+++ DemoProject/src/testpack/Assignment2.java
@@ -0,0 +1,20 @@
+package testpack;
+
+public class Assignment2 {
+	public int Arithmatic (int a, int b, int c, int d, int e, int f ) {
+		int r2= (((((a+b)+c)-d)*e)/f);
+		System.out.println("Display from  Method Arrithmatic  is " +r2);
+		return r2;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Assignment2 objao1 = new Assignment2();
+		int assignment2 = objao1.Arithmatic(10, 2, 2, 2, 2, 2);
+		System.out.println("Display from Main Method assignment2 " +assignment2);
+		System.out.println("Direct calcultion in Print "+((((((10+2)+2)-2)*2)/2)));
+		System.out.println(12);
+
+	}
+
+}
---
+++ DemoProject/src/testpack/TestClass.java
@@ -0,0 +1,40 @@
+package testpack;
+
+public class TestClass {
+	
+	public int sum(int a1,int b1) {
+		int s1=a1+b1;
+		System.out.println("Sum result is " +s1);
+		return s1;	
+		
+	}
+	public int sub(int a2,int b2) {
+		int s2=a2-b2;
+		System.out.println("Subtraction result is " +s2);
+		return s2;
+	}
+	public int mul(int a3,int b3) {
+		int s3=a3*b3;
+		System.out.println("Multiplication result is " +s3);
+		return s3;		
+	}
+	public int dev(int a4,int b4) {
+		int s4= a4/b4;
+		System.out.println("Division result is " +s4);
+		return s4;		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		TestClass tc = new TestClass();
+		int sum_result1= tc.sum(10 ,2);
+		int sum_result2= tc.sum(sum_result1 ,2);
+		int sub_result=tc.sub(sum_result2,2);
+		int mul_result =tc.mul(sub_result, 2);
+		int dev_result=tc.dev(mul_result, 2);
+		System.out.printf("The final answer is " + dev_result);
+			
+
+	}
+
+}
